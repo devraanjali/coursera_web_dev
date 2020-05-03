d4849388680a5d23b1d4710a84b13fcbb59968b5
devraanjali <danjali@iitk.ac.in>
2020-05-03 16:03:24 +0530

    module 4 added

---
+++ module4/script.js
@@ -0,0 +1,15 @@
+
+(function(){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i in names){
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter=== 'j') {
+      bye.speak(names[i]);
+    } else {
+      hello.speak(names[i]);
+    }
+  }
+
+
+})();
+
